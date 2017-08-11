078c884ff3050bee5ebe36c7074b4113c4c512f2
Chenglu Li <kabelee92@gmail.com>
2017-08-10 20:54:58 -0500

    Implemented cors

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 var express = require ("express");
 var bodyParser= require("body-parser");
+var morgan = require("morgan")
 
 var PORT = process.env.PORT || 3000;
 var app = express();
 var router = require('./controllers/api.js');
 var db = require ("./models");
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.text());
 app.use(express.static('public'));
+app.use(allowCrossDomain);
 app.use('/api', router);
 app.use('/', function (req, res){
     res.render('./views/index.html')
 });
+
+app.use(function(err, req, res, next) {
+    console.log(err);
+    next();
+})
 
 db.sequelize.sync().then(function(){
     app.listen(PORT, function() {
